b902c7cd8f0f1acc3ddab3f07686023972f821c8
Kevin Decena <111836807+kevindecena@users.noreply.github.com>
2023-05-23 13:37:28 +0800

    Add files via upload
    complete backend with Spring Boot

---
+++ 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -0,0 +1,34 @@
+package com.in28minutes.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class RestfulWebServicesApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(RestfulWebServicesApplication.class, args);
+    }
+
+    //https://localhost:3000/ to 8080 (Cross Origin Request)
+    //Cross Origin Request
+    //allow all request only from https://localhost:3000/
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("*")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+
+    }
+
+}
